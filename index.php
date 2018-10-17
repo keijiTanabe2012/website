61e01b630b3cbaeacf6156ac24cb0f9a689192f2
KeijiTanabe2012 <yff68116@nifty.com>
2018-10-18 00:34:13 +0900

    表紙ページのひな型
    表紙ページのひな型をつくった。

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html;charset=EUC-JP">
+    <title>TOP - 窮理至命</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script type="text/x-mathjax-config">
+        MathJax.Hub.Config({ tex2jax: { inlineMath: [['$','$'], ["\\(","\\)"]] } });
+      </script>
+      <script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_CHTML">
+      </script>
+      <meta http-equiv="X-UA-Compatible" CONTENT="IE=EmulateIE7" />
+    <script src="main.js"></script>
+</head>
+<body>
+<div id="wrap">
+<?php
+    include("./header.html")
+?>
+<article>
+<?php
+    include("./top_body.html")
+?>
+</article>
+</div>
+</body>
+</html>
---
+++ math/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>TOP - 窮理至命</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="/main.css" />
+    <script type="text/x-mathjax-config">
+        MathJax.Hub.Config({ tex2jax: { inlineMath: [['$','$'], ["\\(","\\)"]] } });
+      </script>
+      <script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_CHTML">
+      </script>
+      <meta http-equiv="X-UA-Compatible" CONTENT="IE=EmulateIE7" />
+    <script src="main.js"></script>
+</head>
+<body>
+<div id="wrap">
+<?php
+    include("./header.html")
+?>
+<article>
+<?php
+    include("./math_body.html")
+?>
+</article>
+</div>
+</body>
+</html>
